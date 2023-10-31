20832878e24af2ca601d2f2041307582e093857d
Edison Vargas <helpedison@gmail.com>
2023-10-30 20:49:05 -0500

    project master

---
+++ src/main/java/com/reservation/hotelsJuan/mgnreservation/entity/Hotel.java
@@ -0,0 +1,13 @@
+package com.reservation.hotelsJuan.mgnreservation.entity;
+
+import java.time.LocalDate;
+
+public class Hotel {
+
+    private String id;
+    private String name;
+    private int numberSuits;
+    private String address;
+    private LocalDate dateInit;
+
+}
---
+++ src/main/java/com/reservation/hotelsJuan/mgnreservation/entity/Suite.java
@@ -0,0 +1,15 @@
+package com.reservation.hotelsJuan.mgnreservation.entity;
+import java.time.LocalDate;
+
+public class Suite {
+
+        private String id;
+        private String name;
+        private int number;
+        private String status;
+        private String hotelId;
+        private int price;
+        private LocalDate dateStatus;
+
+        // Getters y setters
+}
